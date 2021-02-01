69c1da3d5e9ceb45f25101d216260b86dd5c10eb
HeAohow <1131874436@qq.com>
2021-02-01 17:02:08 +0800

    initial commit

--- GeoQuiz/app/src/main/java/com/heao/geoquiz/MainActivity.java
+++ GeoQuiz/app/src/main/java/com/heao/geoquiz/MainActivity.java
@@ -3,12 +3,37 @@ package com.heao.geoquiz;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.Gravity;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
+    private Button mTrueButton;
+    private Button mFalseButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        mTrueButton = findViewById(R.id.true_button);
+        mFalseButton = findViewById(R.id.false_button);
+
+        Toast mTrueToast = Toast.makeText(MainActivity.this, R.string.correct_toast, Toast.LENGTH_SHORT);
+        Toast mFalseToast = Toast.makeText(MainActivity.this, R.string.incorrect_toast, Toast.LENGTH_SHORT);
+
+        mTrueButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                mTrueToast.setGravity(Gravity.TOP, 0, 0);
+                mTrueToast.show();
+            }
+        });
+
+        mFalseButton.setOnClickListener(v -> {
+            mFalseToast.setGravity(Gravity.TOP, 0, 0);
+            mFalseToast.show();
+        });
     }
 }
