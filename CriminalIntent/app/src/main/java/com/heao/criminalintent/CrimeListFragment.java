2474d6e83a423c337ffaacaccab05a2557951eb5
HeAohow <1131874436@qq.com>
2021-02-03 21:06:43 +0800

    chap-11 completed;

--- CriminalIntent/app/src/main/java/com/heao/criminalintent/CrimeListFragment.java
+++ CriminalIntent/app/src/main/java/com/heao/criminalintent/CrimeListFragment.java
@@ -56,7 +56,8 @@ public class CrimeListFragment extends Fragment {
 
         @Override
         public void onClick(View view) {
-            Intent intent = MainActivity.newIntent(getActivity(), mCrime.getId());
+//            Intent intent = MainActivity.newIntent(getActivity(), mCrime.getId());
+            Intent intent = CrimePagerActivity.newIntent(getActivity(), mCrime.getId());
             startActivityForResult(intent, REQUEST_CRIME);
         }
     }
---
+++ CriminalIntent/app/src/main/java/com/heao/criminalintent/CrimePagerActivity.java
@@ -0,0 +1,82 @@
+package com.heao.criminalintent;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentStatePagerAdapter;
+import androidx.viewpager2.adapter.FragmentStateAdapter;
+import androidx.viewpager2.widget.ViewPager2;
+
+import android.content.Context;
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+import java.util.List;
+import java.util.UUID;
+
+public class CrimePagerActivity extends AppCompatActivity {
+    private static final String EXTRA_CRIME_ID = "com.heao.criminalintent.crime_id";
+    private ViewPager2 mViewPager;
+    private Button mToFirstButton;
+    private Button mToLastButton;
+    private List<Crime> mCrimes;
+
+    public static Intent newIntent(Context packageContext, UUID crimeId) {
+        Intent intent = new Intent(packageContext, CrimePagerActivity.class);
+        intent.putExtra(EXTRA_CRIME_ID, crimeId);
+        return intent;
+    }
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_crime_pager);
+        UUID crimeId = (UUID) getIntent().getSerializableExtra(EXTRA_CRIME_ID);
+
+        mViewPager = findViewById(R.id.crime_view_pager);
+        mToFirstButton = findViewById(R.id.to_first_button);
+        mToLastButton = findViewById(R.id.to_last_button);
+
+        mCrimes = CrimeLab.get(this).getCrimes();
+
+        mViewPager.setAdapter(new FragmentStateAdapter(this) {
+            @NonNull
+            @Override
+            public Fragment createFragment(int position) {
+                Crime crime = mCrimes.get(position);
+                return CrimeFragment.newInstance(crime.getId());
+            }
+
+            @Override
+            public int getItemCount() {
+                return mCrimes.size();
+            }
+        });
+        mViewPager.registerOnPageChangeCallback(new ViewPager2.OnPageChangeCallback() {
+            @Override
+            public void onPageSelected(int position) {
+                super.onPageSelected(position);
+                mToFirstButton.setEnabled(true);
+                mToLastButton.setEnabled(true);
+                if (position == 0) {
+                    mToFirstButton.setEnabled(false);
+                } else if (position == mCrimes.size() - 1) {
+                    mToLastButton.setEnabled(false);
+                }
+            }
+        });
+        mToFirstButton.setOnClickListener((v) -> mViewPager.setCurrentItem(0));
+        mToLastButton.setOnClickListener((v) -> mViewPager.setCurrentItem(mCrimes.size() - 1));
+
+        // 将CrimePage定位到之前点击的item页面
+        for (int i = 0; i < mCrimes.size(); i++) {
+            if (mCrimes.get(i).getId().equals(crimeId)) {
+                mViewPager.setCurrentItem(i);
+                break;
+            }
+        }
+    }
+}
